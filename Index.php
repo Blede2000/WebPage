d90f3fa4b600e75e1818a77ad7390f3f54b97047
Blede2000 <maxtergameplay@gmail.com>
2015-02-21 20:42:53 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -5,32 +5,65 @@
   	<meta name="description" content="Esta es una Pagina con noticias de la UNAD">
   	<meta name="keywords" content="UNAD, Carlos Eduardo Alvarez Montoya, Diseño web, HTML">
   	<link rel="stylesheet" type="text/css" href="css/IndexStyle.css"/>
-    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
   	<title>Carlos Eduardo Alvarez Montoya | UNAD New</title>
   </head>
   <body>
+  <div class="row">
   	<header>
     <div class="cabezado">
   		<!--Aqui mas cosas del Header-->
-      UNAD- Carlos Eduardo 
+      <a href="#">UNAD- Carlos Eduardo Alvarez Montoya</a> 
     </div>
   	</header>
   	<section>
-  		<div class="SS">
-  			<h1>AQUI EL TITULO</h1>
-  		</div>
+  		<div class="SS col-sm-8">
+        <h1>Noticias De la UNAD</h1>
+        <div class="noticias">
+  			<h1>UNAD es cuestionada por abrir sede en EE.UU</h1>
+        <p>En 2001, la Universidad Nacional Abierta y a Distancia, una institución pública, decidió abrir una sede en Estados Unidos con el objetivo inicial de ofrecer programas académicos a los más de 8.000 presos colombianos confinados en las cárceles de ese país.</p>
+        </br>
+        <p>Ahora, más de una década después de haber iniciado su operación en el exterior, las directivas de la institución enfrentan duros señalamientos por parte de la Contraloría General de la República.</p>
+        </br>
+        <p>En un comunicado público, el ente de control cuestionó la actuación de las directivas y la inversión por $6.230 millones que realizaron para crear una sede en Florida (Estados Unidos) “de naturaleza privada y regida por las leyes y normas de ese país”.
+
+Según la Contraloría, tanto la junta directiva como el rector de la UNAD traspasaron el marco de las limitaciones establecidas. “Teniendo en cuenta que la sede en Florida es una corporación sin ánimo de lucro registrada y domiciliada en los Estados Unidos de Norte América, se evidenció un detrimento por $5.939,93 millones, por desembolsar recursos de la Nación para la creación y el mantenimiento de un tercero particular”, señaló el ente de control.</p>
+        <img src="http://www.elespectador.com/files/imagecache/560_width_display/img_ipad/0d5a95a74b994821d0f2046db9ee7c85.jpg" class="responsive">
+        <p>Además de esta inversión, la Contraloría cuestionó el pago con recursos públicos de viáticos y comisiones a Florida para el rector y otros funcionarios, como también a empleados de esa sede, constituyendo un detrimento patrimonial en cuantía superior a los $290 millones.</p>
+        </br>
+        <p>La asesora jurídica de la UNAD, Ángela Melo, le explicó a El Espectador que la sede de la universidad en Florida se abrió por petición de la Cancillería, con el propósito inicial de atender a la población colombiana en las cárceles de Estados Unidos, y más adelante incluyó a la población latina en la región.</p>
+        </div>
+        <div class="noticias">
+          <h1>Acusaciones malintencionadas buscan afectar dignidad de la institución': UNAD</h1>
+          <p>La Universidad Nacional Abierta y a Distancia (UNAD) desestimó los señalamientos del Ministerio de Educación sobre presuntas irregularidades administrativas y académicas.</p>
+          </br>
+          <p>A través de un comunicado de prensa, la UNAD declaró que las acusaciones sobre malos manejos administrativos “buscan deslegitimar la misión y el compromiso” que tiene la institución con la educación del país.</p>
+          </br>
+          <p>“Los rumores que circulan sobre el particular son totalmente infundados y han sido promovidos por personas identificadas, que de manera malintencionada, buscan afectar la dignidad de (la comunidad educativa), y la bien ganada reputación institucional durante sus 33 años de existencia. Todo ello gestado en el contexto que se adelanta durante el actual proceso de designación rectoral”, indicó la UNAD</p>
+          </br>
+          <p>Reiterando que no ha sido objeto de ninguna sanción por parte del Ministerio de Educación, ni de ningún organismo de control (Procuraduría, Fiscalía o Contraloría), la Universidad declaró que “goza de plena estabilidad académica y administrativa”.
+
+“La UNAD responde de manera transparente por su sanidad y sostenibilidad financiera; razón por la cual cumple a cabalidad y oportunamente con la totalidad de sus obligaciones laborales y contractuales con el personal académico y administrativo”, agregó la institución.</p>
+        </div>
+        </div>
   	</section>
   	<aside>
-  		<div class="asidestyle">
-  			<!--Aqui otra informacion relevante-->
-        <p>Holi aqui va un lorem Ipsum</p>
+  		<div id="StyleAside" class="col-sm-4">
+        <div id="tweets">
+        <h3>Tweets de @UniversidadUNAD</h3>
+         <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/UniversidadUNAD" data-widget-id="569301005872160768">Tweets por el @UniversidadUNAD.</a>
+              <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
   		</div>
+      <div>
+        <h2>Facebook</h2>
+      </div>
   	</aside>
   	<footer>
-  		<div id="footerstyle">
+  		<div id="footerstyle" class="col-md-12">
   			<!--Redes Sociales y otras aqui-->
         <p>REDES Y COSAS AQUI</p>
   		</div>
   	</footer>
+    </div>
   </body>
 </html>
